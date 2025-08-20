b8572d04651a40898b65183e096c86275e50d0ba
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2025-08-20 13:25:20 +0300

    Move Compose functions into MainActivity class, add string resource in text field supporting text

--- app/src/main/java/com/example/simplecontactlist/MainActivity.kt
+++ app/src/main/java/com/example/simplecontactlist/MainActivity.kt
@@ -22,14 +22,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -44,7 +42,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.getString
 import com.example.simplecontactlist.ui.theme.SimpleContactListTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,137 +64,141 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun MainScreen() {
-    ContactsList()
-}
+    @Composable
+    fun MainScreen() {
+        ContactsList()
+    }
 
-@Composable
-fun ContactsList() {
-    // А как обрабатывать смену состояния? Типа, экран перевернул?
-    // не обязательно юзать mutableStateListOf. попробуй metableStateOf а внутри лист строк.
-    val listItems = remember { mutableStateListOf<String>()}
-    var text by remember { mutableStateOf("") }
-    var errorText by remember { mutableStateOf("") }
-    // val insets = rememberKeyboardInsets() - что это за дичь из нейросетей?
+    @Composable
+    fun ContactsList() {
+        // А как обрабатывать смену состояния? Типа, экран перевернул?
+        // не обязательно юзать mutableStateListOf. попробуй metableStateOf а внутри лист строк.
+        val listItems = remember { mutableStateListOf<String>()}
+        var text by remember { mutableStateOf("") }
+        var errorText by remember { mutableStateOf("") }
+        // val insets = rememberKeyboardInsets() - что это за дичь из нейросетей?
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        LazyColumn(
-            contentPadding = PaddingValues(all = 0.dp),
-            modifier = Modifier.height(350.dp)
-
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (listItems.isNotEmpty()) {
-            items(listItems) { item ->
-                Row(
-                    horizontalArrangement = Arrangement.Absolute.Left,
-                    modifier = Modifier
-                        .padding(start = 12.dp, end = 12.dp)
-                        .fillMaxWidth()
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .size(40.dp)
-                            .clip(CircleShape)
-                            .background(
-                                brush = Brush.radialGradient(
-                                    colors = listOf(
-                                        Color.Red,
-                                        Color.Blue
-                                    )
-                                )
-                            )
-                    )
-                    Spacer(modifier = Modifier.width(100.dp))
-                    Text(
-                        text = item,
-                        modifier = Modifier.width(200.dp)
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                    Button(
-                        onClick = { listItems.remove(item) },
-                        modifier = Modifier,
-                        shape = MaterialTheme.shapes.extraSmall,
-                        contentPadding = PaddingValues(
-                            start = 2.dp,
-                            top = 2.dp,
-                            end = 2.dp,
-                            bottom = 2.dp
-                        )
-                    ) {
-                        Text(stringResource(R.string.delete_button_text))
-                    }
-                }
-            }
-            }
-            // Такой вариант - очень плохой, кмк. Как это обойти? Проблема с композабл скоупом.
-            // Вопрос: можно ли менять параметры модифаера композ функции на ходу, вне модифаера? Например, хочу что б в if был lazy column в рамке, а в else - без рамки.
-            else {
-                // Items использован что бы задать composable скоуп и иметь возможность добавить изображение emptyState'а.
-                items(arrayListOf("")) {
-                    Column {
-                        Image(painter = painterResource(id = R.drawable.emptystate2),
-                            contentDescription = "EmptyState",
+            LazyColumn(
+                contentPadding = PaddingValues(all = 0.dp),
+                modifier = Modifier.height(350.dp)
+
+            ) {
+                if (listItems.isNotEmpty()) {
+                    items(listItems) { item ->
+                        Row(
+                            horizontalArrangement = Arrangement.Absolute.Left,
                             modifier = Modifier
-                                .size(170.dp)
-                                .clip(CircleShape)
-                        )
-                        Text(
-                            text = stringResource(R.string.empty_state_text),
-                            modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
+                                .padding(start = 12.dp, end = 12.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .size(40.dp)
+                                    .clip(CircleShape)
+                                    .background(
+                                        brush = Brush.radialGradient(
+                                            colors = listOf(
+                                                Color.Red,
+                                                Color.Blue
+                                            )
+                                        )
+                                    )
+                            )
+                            Spacer(modifier = Modifier.width(100.dp))
+                            Text(
+                                text = item,
+                                modifier = Modifier.width(200.dp)
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Button(
+                                onClick = { listItems.remove(item) },
+                                modifier = Modifier,
+                                shape = MaterialTheme.shapes.extraSmall,
+                                contentPadding = PaddingValues(
+                                    start = 2.dp,
+                                    top = 2.dp,
+                                    end = 2.dp,
+                                    bottom = 2.dp
+                                )
+                            ) {
+                                Text(stringResource(R.string.delete_button_text))
+                            }
+                        }
+                    }
+                }
+                // Такой вариант - очень плохой, кмк. Как это обойти? Проблема с композабл скоупом.
+                // Вопрос: можно ли менять параметры модифаера композ функции на ходу, вне модифаера? Например, хочу что б в if был lazy column в рамке, а в else - без рамки.
+                else {
+                    // Items использован что бы задать composable скоуп и иметь возможность добавить изображение emptyState'а.
+                    items(arrayListOf("")) {
+                        Column {
+                            Image(painter = painterResource(id = R.drawable.emptystate2),
+                                contentDescription = "EmptyState",
+                                modifier = Modifier
+                                    .size(170.dp)
+                                    .clip(CircleShape)
+                            )
+                            Text(
+                                text = stringResource(R.string.empty_state_text),
+                                modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
+                        }
                     }
                 }
             }
-        }
 
-        OutlinedTextField(
-            value = text,
-            onValueChange = {
-                newText -> text = newText
-                errorText = ""
-            },
-            modifier = Modifier
-                .padding(start = 10.dp, end = 10.dp, top = 50.dp)
-                .width(200.dp)
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth(),
-            label = { Text(stringResource(R.string.Add_contact_text_field_label)) },
-            supportingText = {
-                if (errorText.isNotEmpty()) {
-                    Text(
-                        text = errorText,
-                        color = Color.Red
-                    )
+            OutlinedTextField(
+                value = text,
+                onValueChange = {
+                        newText -> text = newText
+                    errorText = ""
+                },
+                modifier = Modifier
+                    .padding(start = 10.dp, end = 10.dp, top = 50.dp)
+                    .width(200.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(),
+                label = { Text(stringResource(R.string.Add_contact_text_field_label)) },
+                supportingText = {
+                    if (errorText.isNotEmpty()) {
+                        Text(
+                            text = errorText,
+                            color = Color.Red
+                        )
+                    }
                 }
-            }
-        )
-        Button(
-            modifier = Modifier.padding(top = 0.dp),
-            onClick = {
-                if (text.isNotBlank()) {
-                    listItems.add(text)
-                    text = ""
-                } else {
-                    // Как сюда запихнуть ресурс?
-                    errorText = "Field is empty. Enter name"
+            )
+            Button(
+                modifier = Modifier.padding(top = 0.dp),
+                onClick = {
+                    if (text.isNotBlank()) {
+                        listItems.add(text)
+                        text = ""
+                    } else {
+                        // Как сюда запихнуть ресурс?
+                        errorText = this@MainActivity.getString(R.string.emty_name_field_error) // "Field is empty. Enter name"
+                    }
                 }
+            ) { Text(stringResource(R.string.add_contact_button_text)) }
+        }
+    }
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @Preview(showBackground = true)
+    @Composable
+    fun ContactList() {
+        Scaffold {
+            SimpleContactListTheme {
+                MainScreen()
             }
-        ) { Text(stringResource(R.string.add_contact_button_text)) }
+        }
     }
 }
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Preview(showBackground = true)
-@Composable
-fun ContactList() {
-    Scaffold {
-        SimpleContactListTheme {
-            MainScreen()
-        }
-    }
-}
+
+
+
