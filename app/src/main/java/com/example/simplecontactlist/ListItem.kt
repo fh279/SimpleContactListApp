6728bed89175c9af44c7c398de808cb66b34380d
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2025-08-27 18:00:35 +0300

    Add data class for list item

---
+++ app/src/main/java/com/example/simplecontactlist/ListItem.kt
@@ -0,0 +1,13 @@
+package com.example.simplecontactlist
+
+import java.util.UUID
+
+data class ListItem(
+    val name: String,
+    val surname: String,
+    val number: String,
+    val id: UUID = UUID.randomUUID(),
+    val color: Float
+) {
+
+}
