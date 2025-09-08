3750b64876e4df31524b6d7e6c1b480a4e3a00e8
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2025-09-08 21:19:58 +0300

    Optimize layout and separate it on different compose functions

--- app/src/main/java/com/example/simplecontactlist/MainActivity.kt
+++ app/src/main/java/com/example/simplecontactlist/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.simplecontactlist
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.view.WindowInsets.Type.ime
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -11,6 +10,7 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,12 +18,9 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imeNestedScroll
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -73,219 +70,235 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleContactListTheme {
                 Scaffold(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .imeNestedScroll()
-                ) { innerPadding -> MainScreen(Modifier
-                    .fillMaxSize()
-                    .padding()) }
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    MainScreen(Modifier.padding(innerPadding))
+                }
             }
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
     @Composable
-    fun MainScreen(
-        modifier: Modifier = Modifier
-    ) {
-
+    fun MainScreen(modifier: Modifier) {
         val state: State<ListState> = viewModel.state.collectAsStateWithLifecycle()
         val listState: ListState = state.value
-        ContactsList(modifier, listState)
-    }
 
-    @RequiresApi(Build.VERSION_CODES.R)
-    @Composable
-    fun ContactsList(modifier: Modifier = Modifier, state: ListState) {
         Column(
             modifier = modifier
                 .verticalScroll(rememberScrollState())
-                .padding(vertical = ime().dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .padding(all = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            LazyColumn(
-                state = rememberLazyListState(),
-                modifier = Modifier
-                    .padding(
-                        start = 10.dp,
-                        end = 10.dp
-                    )
-                    .height(350.dp)
-                    .border(
-                        width = 1.dp,
-                        color = Color.Black,
-                        shape = RoundedCornerShape(5.dp)
-                    )
-                    .background(color = Color.White, shape = RoundedCornerShape(16.dp))
-            ) {
-                if (state.listItems.isNotEmpty()) {
-                    items(state.listItems,
-                        key = { it.id }
-                    ) { item ->
-                        val color = remember {
-                            Color(
-                                red = Random.nextFloat(),
-                                green = Random.nextFloat(),
-                                blue = Random.nextFloat()
-                            )
-                        }
+            ContactList(
+                modifier = modifier,
+                state = listState
+            )
+            Spacer(Modifier.height(20.dp))
+            AddContact(
+                modifier = modifier,
+                state = listState
+            )
+        }
+    }
 
-                        Row(
-                            horizontalArrangement = Arrangement.Absolute.Left,
+    @Composable
+    fun ContactList(
+        modifier: Modifier,
+        state: ListState
+    ) {
+        LazyColumn(
+            state = rememberLazyListState(),
+            modifier = modifier
+                .fillMaxSize()
+                .height(350.dp)
+                .border(
+                    width = 1.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(5.dp)
+                )
+                .background(color = Color.White, shape = RoundedCornerShape(16.dp))
+        ) {
+            if (state.listItems.isNotEmpty()) {
+                items(
+                    state.listItems,
+                    key = { it.id }
+                ) { item ->
+                    val color = remember {
+                        Color(
+                            red = Random.nextFloat(),
+                            green = Random.nextFloat(),
+                            blue = Random.nextFloat()
+                        )
+                    }
+
+                    Row(
+                        horizontalArrangement = Arrangement.Absolute.Left,
+                        modifier = Modifier
+                            .padding(start = 12.dp, end = 12.dp, top = 10.dp)
+                            .fillMaxWidth()
+                            .background(color = Color.White, shape = RoundedCornerShape(5.dp)),
+                    ) {
+                        Box(
                             modifier = Modifier
-                                .padding(start = 12.dp, end = 12.dp, top = 10.dp)
-                                .fillMaxWidth()
-                                .background(color = Color.White, shape = RoundedCornerShape(5.dp)),
+                                .align(Alignment.CenterVertically)
+                                .size(40.dp)
+                                .clip(CircleShape)
+                                .background(color = color),
+                            contentAlignment = Alignment.Center
                         ) {
-                            Box(
-                                modifier = Modifier
-                                    .align(Alignment.CenterVertically)
-                                    .size(40.dp)
-                                    .clip(CircleShape)
-                                    .background(color = color),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Text(
-                                    text = item.name.getOrNull(0).toString(),
-                                    color = Color.White,
-                                    modifier = Modifier.align(Alignment.Center),
-                                    fontSize = 20.sp,
-                                    fontWeight = FontWeight.Bold
-                                )
-                            }
-                            Spacer(modifier = Modifier.width(10.dp))
-                            Column(modifier = Modifier.width(250.dp)) {
-                                Text(text = "${item.name} ${item.surname}")
-                                Text(text = item.number)
-                            }
-                            Button(
-                                onClick = { viewModel.removeItemFromList(item) },
-                                modifier = Modifier,
-                                shape = MaterialTheme.shapes.extraSmall,
-                                contentPadding = PaddingValues(
-                                    start = 2.dp,
-                                    top = 2.dp,
-                                    end = 2.dp,
-                                    bottom = 2.dp
-                                )
-                            ) { Text(stringResource(R.string.delete_button_text)) }
+                            Text(
+                                text = item.name.getOrNull(0).toString(),
+                                color = Color.White,
+                                modifier = Modifier.align(Alignment.Center),
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
                         }
-                    }
-                }
-                else {
-                    items(arrayListOf("")) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier.padding(all = 50.dp)
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.emptystate2),
-                                contentDescription = "EmptyState",
-                                modifier = Modifier
-                                    .size(170.dp)
-                                    .clip(CircleShape),
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Column(modifier = Modifier.width(250.dp).horizontalScroll(rememberScrollState())) {
+                            Text(
+                                text = "${item.name} ${item.surname}",
+                                maxLines = 1,
                             )
                             Text(
-                                text = stringResource(R.string.empty_state_text),
-                                modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
+                                text = item.number,
+                                maxLines = 1
+                            )
                         }
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Button(
+                            onClick = { viewModel.removeItemFromList(item) },
+                            modifier = Modifier,
+                            shape = MaterialTheme.shapes.extraSmall,
+                            contentPadding = PaddingValues(
+                                start = 2.dp,
+                                top = 2.dp,
+                                end = 2.dp,
+                                bottom = 2.dp
+                            )
+                        ) { Text(stringResource(R.string.delete_button_text)) }
+                    }
+                }
+            } else {
+                items(arrayListOf("")) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.emptystate2),
+                            contentDescription = "EmptyState",
+                            modifier = Modifier
+                                .size(170.dp)
+                                .clip(CircleShape),
+                        )
+                        Text(
+                            text = stringResource(R.string.empty_state_text),
+                            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                        )
                     }
                 }
             }
+        }
+    }
 
-            Column(
+    @Composable
+    fun AddContact(
+        state: ListState,
+        modifier: Modifier
+    ) {
+        Column(
+            modifier = modifier
+                .animateContentSize()
+                .border(
+                    width = 1.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(all = 20.dp)
+        ) {
+            Text(
+                text = "Add new contact",
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+                fontWeight = FontWeight.Bold
+            )
+            OutlinedTextField(
+                value = state.name,
+                onValueChange = { name ->
+                    viewModel.onChangeNameFieldValue(name)
+                    viewModel.clearErrorText()
+                },
                 modifier = Modifier
-                    .animateContentSize()
-                    .padding(top = 10.dp, start = 10.dp, end = 10.dp)
-                    .border(
-                        width = 1.dp,
-                        color = Color.Black,
-                        shape = RoundedCornerShape(16.dp)
-                    )
-                    .background(
-                        color = Color.White,
-                        shape = RoundedCornerShape(16.dp)
-                    )
-                    .padding(all = 20.dp)
-            ) {
-                Text(
-                    text = "Add new contact",
-                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
-                    fontWeight = FontWeight.Bold
-                )
-                OutlinedTextField(
-                    value = state.name,
-                    onValueChange = {
-                        name -> viewModel.onChangeNameFieldValue(name)
-                        viewModel.clearErrorText()
-                    },
-                    modifier = Modifier
-                        .width(250.dp)
-                        .align(Alignment.CenterHorizontally)
-                        .fillMaxWidth(),
-                    label = { Text(stringResource(R.string.Add_contact_name_field_label)) },
-                    supportingText = {
-                        if (state.name.isEmpty()) {
-                            Text(
-                                text = state.errorText ?: "",
-                                color = Color.Red
-                            )
-                        }
-                    },
-                    maxLines = 1
-                )
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(),
+                label = { Text(stringResource(R.string.Add_contact_name_field_label)) },
+                supportingText = {
+                    if (state.name.isEmpty()) {
+                        Text(
+                            text = state.errorText ?: "",
+                            color = Color.Red
+                        )
+                    }
+                },
+                maxLines = 1
+            )
 
-                OutlinedTextField(
-                    value = state.surName,
-                    onValueChange = {
-                        surName -> viewModel.onChangeSurnameValue(surName)
-                        viewModel.clearErrorText()
-                    },
-                    modifier = Modifier
-                        .width(250.dp)
-                        .align(Alignment.CenterHorizontally)
-                        .fillMaxWidth(),
-                    label = { Text(stringResource(R.string.Add_contact_surname_field_label)) },
-                    supportingText = {
-                        if (state.surName.isEmpty()) {
-                            Text(
-                                text = state.errorText ?: "",
-                                color = Color.Red
-                            )
-                        }
-                    },
-                    maxLines = 1
-                )
+            OutlinedTextField(
+                value = state.surName,
+                onValueChange = { surName ->
+                    viewModel.onChangeSurnameValue(surName)
+                    viewModel.clearErrorText()
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(),
+                label = { Text(stringResource(R.string.Add_contact_surname_field_label)) },
+                supportingText = {
+                    if (state.surName.isEmpty()) {
+                        Text(
+                            text = state.errorText ?: "",
+                            color = Color.Red
+                        )
+                    }
+                },
+                maxLines = 1
+            )
 
-                OutlinedTextField(
-                    value = state.number,
-                    onValueChange = { number -> viewModel.onChangeNumberValue(number)
-                        viewModel.clearErrorText()
-                    },
-                    modifier = Modifier
-                        .width(250.dp)
-                        .align(Alignment.CenterHorizontally)
-                        .fillMaxWidth(),
-                    label = { Text(stringResource(R.string.Add_contact_number_field_label)) },
-                    supportingText = {
-                        if (state.number.isEmpty()) {
-                            Text(
-                                text = state.errorText ?: "",
-                                color = Color.Red
-                            )
-                        }
-                    },
-                    maxLines = 1
-                )
+            OutlinedTextField(
+                value = state.number,
+                onValueChange = { number ->
+                    viewModel.onChangeNumberValue(number)
+                    viewModel.clearErrorText()
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(),
+                label = { Text(stringResource(R.string.Add_contact_number_field_label)) },
+                supportingText = {
+                    if (state.number.isEmpty()) {
+                        Text(
+                            text = state.errorText ?: "",
+                            color = Color.Red
+                        )
+                    }
+                },
+                maxLines = 1
+            )
 
-                val text = stringResource(R.string.emty_name_field_error)
-                Button(
-                    modifier = Modifier
-                        .align(alignment = Alignment.CenterHorizontally),
-                    onClick = { viewModel.addItemToList(emptyStateStringResource = text) }
-                ) { Text(stringResource(R.string.add_contact_button_text)) }
+            val text = stringResource(R.string.emty_name_field_error)
+            Button(
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally),
+                onClick = { viewModel.addItemToList(emptyStateStringResource = text) }
+            ) { Text(stringResource(R.string.add_contact_button_text)) }
 
-            }
         }
     }
 
@@ -295,7 +308,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun ContactList() {
         SimpleContactListTheme {
-            MainScreen()
+            MainScreen(Modifier)
         }
     }
 }
